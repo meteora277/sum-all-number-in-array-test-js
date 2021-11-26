6ca6ebf3d2f7146f015e8a512983f4c2fee116e8
meteora277 <justin.s.diaz@gmail.com>
2021-11-26 16:45:30 -0500

    defined function sumItems and it's logic

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  let sum = 0;
+
+  array.forEach(item => {
+    //itterating over first loop is a must
+
+    if (item instanceof Array) {
+      //if item in array is an array, it will recursively call sumItem functions with item as an arguement
+      //when it reaches base case it will save to addArray
+      let addArray = [sumItems(item)];
+     
+      for (let item of addArray) {
+
+        sum += item;
+      }
+    
+    } else {
+
+      sum += item;
+    }
+  });
+  return sum;
+};
+
+console.log(sumItems([1,2,3,[4,5]]));
 
 module.exports = sumItems;
